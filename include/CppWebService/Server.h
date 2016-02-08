449e2c73e1ff8d400cc8425cbf6c515e35c73a62
Stefan Peters <baumkronenprinz@googlemail.com>
2016-02-08 22:54:03 +0100

    added the first methods and some real tests

--- include/CppWebService/Server.h
+++ include/CppWebService/Server.h
@@ -1,17 +1,72 @@
 #ifndef CPPWEBSERVICE_SERVER_H
 #define CPPWEBSERVICE_SERVER_H
 
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
 struct mg_connection;
 struct mg_context;
 
-
 struct Connection {
+    Connection(mg_connection*) {}
+};
 
-    Connection(mg_connection *connection);
+class Server {
+
+public:
+
+    struct Exception : public std::runtime_error  {
+        Exception(const std::string &msg) : std::runtime_error(msg) {}
+    };
+
+    Server(std::vector<std::string> options = std::vector<std::string>());
+    virtual ~Server();
+
+    /**
+     * its not allowed to copy the object, because the address of the object
+     * is internally coupled to the civetweb structures
+     */
+    Server(const Server& other) = delete;
+
+    void close();
+    bool listen();
+
+    bool isListening() const;
+    std::vector<int> getListeningPorts() const;
+
+
+    virtual int on_begin_request(Connection connection) {}
+
+    virtual void on_end_request(const Connection connection,
+                        int reply_status_code) const {}
+
+    virtual int on_log_message(const Connection connection, const char *message) {}
+
+    virtual int on_log_access(const Connection connection, const char *message) {}
+
+    virtual int on_init_ssl(void *ssl_context, void *user_data) {}
+
+    virtual void on_connection_close(const Connection connection) {}
+
+    virtual const char *on_open_file(const Connection connection, const char *path,
+                             size_t *data_len) {}
+
+    virtual void on_init_lua(const Connection connection, void *lua_context) {}
+
+    virtual void on_upload(Connection connection, const char *file_name) {}
+
+    virtual int on_http_error(Connection connection, int status) {}
+
+    virtual void on_init_context(const struct mg_context *ctx) {}
+
+    virtual void on_exit_context(const struct mg_context *ctx) {}
 
 
 private:
-    mg_connection *_connection;
+    mg_context* _context;
+    std::vector<std::string> _options;
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,100 +1,120 @@
 #include "Server.h"
 #include <civetweb.h>
 
+using namespace std;
 
-Connection::Connection(mg_connection *connection) : _connection(connection) {}
+namespace {
+
+    inline Server *server(mg_context *context) {
+        return static_cast<Server *>(mg_get_user_data(context));
+    }
+
+    inline const Server *server(const mg_context *context) {
+        return static_cast<const Server *>(mg_get_user_data(context));
+    }
+
+    inline Server *server(mg_connection *connection) {
+        return server(mg_get_context(connection));
+    }
+
+    inline const Server *server(const mg_connection *connection) {
+        return server(mg_get_context(connection));
+    }
 
 
-struct Server {
-    Server() {}
+    int _callback_begin_request(struct mg_connection *connection) {
+        server(connection)->on_begin_request(connection);
+    }
 
-    int on_begin_request(Connection connection) {}
+    void _callback_end_request(const struct mg_connection *connection,
+                               int reply_status_code) {
+        server(connection)
+                ->on_end_request(const_cast<mg_connection *>(connection),
+                                 reply_status_code);
+    }
 
-    void on_end_request(const Connection connection,
-                        int reply_status_code) const {}
+    int _callback_log_message(const struct mg_connection *, const char *message) { }
 
-    int on_log_message(const Connection connection, const char *message) {}
+    int _callback_log_access(const struct mg_connection *, const char *message) { }
 
-    int on_log_access(const Connection connection, const char *message) {}
+    int _callback_init_ssl(void *ssl_context, void *user_data) { }
 
-    int on_init_ssl(void *ssl_context, void *user_data) {}
+    void _callback_connection_close(const struct mg_connection *) { }
 
-    void on_connection_close(const Connection connection) {}
+    const char *_callback_open_file(const struct mg_connection *, const char *path,
+                                    size_t *data_len) { }
 
-    const char *on_open_file(const Connection connection, const char *path,
-                             size_t *data_len) {}
+    void _callback_init_lua(const struct mg_connection *, void *lua_context) { }
 
-    void on_init_lua(const Connection connection, void *lua_context) {}
+    void _callback_upload(struct mg_connection *, const char *file_name) { }
 
-    void on_upload(Connection connection, const char *file_name) {}
+    int _callback_http_error(struct mg_connection *, int status) { }
 
-    int on_http_error(Connection connection, int status) {}
+    void _callback_init_context(const struct mg_context *ctx) { }
 
-    void on_init_context(const struct mg_context *ctx) {}
+    void _callback_exit_context(const struct mg_context *ctx) { }
 
-    void on_exit_context(const struct mg_context *ctx) {}
-
-private:
-    mg_context *_context;
-};
-
-// inline Connection *connection(mg_connection *connection) {
-//  return static_cast<Connection *>(mg_get_user_connection_data(connection));
-//}
-
-inline Server *server(mg_context *context) {
-    return static_cast<Server *>(mg_get_user_data(context));
+    struct mg_callbacks _callbacks = {
+            &_callback_begin_request, &_callback_end_request,
+            &_callback_log_message, &_callback_log_access,
+            &_callback_init_ssl, &_callback_connection_close,
+            &_callback_open_file, &_callback_init_lua,
+            &_callback_upload, &_callback_http_error,
+            &_callback_init_context, &_callback_exit_context};
 }
 
-inline const Server *server(const mg_context *context) {
-    return static_cast<const Server *>(mg_get_user_data(context));
+
+Server::Server(vector<string> options) : _context(nullptr), _options(options) {
+
 }
 
-inline Server *server(mg_connection *connection) {
-    return server(mg_get_context(connection));
+
+Server::~Server() {
+    close();
 }
 
-inline const Server *server(const mg_connection *connection) {
-    return server(mg_get_context(connection));
+
+void Server::close() {
+
+    if(_context) {
+        mg_stop(_context);
+        _context = nullptr;
+    }
 }
 
-int _callback_begin_request(struct mg_connection *connection) {
-    server(connection)->on_begin_request(connection);
+bool Server::listen() {
+
+    if(_context) {
+        throw Exception("called listen() on a already running server");
+    }
+
+    vector<const char *> c_options(_options.size());
+
+    for (auto option : _options) {
+        c_options.push_back(option.c_str());
+    }
+
+    c_options.push_back('\0');
+
+    _context = mg_start(&_callbacks, this, c_options.data());
+
+    return isListening();
 }
 
-void _callback_end_request(const struct mg_connection *connection,
-                           int reply_status_code) {
-    server(connection)
-            ->on_end_request(const_cast<mg_connection *>(connection),
-                             reply_status_code);
+
+bool Server::isListening() const {
+    return _context != nullptr;
 }
 
-int _callback_log_message(const struct mg_connection *, const char *message) {}
 
-int _callback_log_access(const struct mg_connection *, const char *message) {}
+std::vector<int> Server::getListeningPorts() const {
 
-int _callback_init_ssl(void *ssl_context, void *user_data) {}
+    const int SIZE = 64;
 
-void _callback_connection_close(const struct mg_connection *) {}
+    std::vector<int> ports(SIZE);
+    std::vector<int> ssl(SIZE);
 
-const char *_callback_open_file(const struct mg_connection *, const char *path,
-                                size_t *data_len) {}
+    ports.resize(mg_get_ports(_context, ports.size(), &ports[0], &ssl[0]));
 
-void _callback_init_lua(const struct mg_connection *, void *lua_context) {}
-
-void _callback_upload(struct mg_connection *, const char *file_name) {}
-
-int _callback_http_error(struct mg_connection *, int status) {}
-
-void _callback_init_context(const struct mg_context *ctx) {}
-
-void _callback_exit_context(const struct mg_context *ctx) {}
-
-struct mg_callbacks _callbacks = {
-        &_callback_begin_request, &_callback_end_request,
-        &_callback_log_message,   &_callback_log_access,
-        &_callback_init_ssl,      &_callback_connection_close,
-        &_callback_open_file,     &_callback_init_lua,
-        &_callback_upload,        &_callback_http_error,
-        &_callback_init_context,  &_callback_exit_context};
-
+    return ports;
+}
